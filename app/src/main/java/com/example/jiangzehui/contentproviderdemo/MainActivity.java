c65a41d0ace488249428b20b93a95408d3e3abb1
jiangzehui <383001264@qq.com>
2017-11-16 17:41:40 +0800

    2017年11月16日17:41:34

--- app/src/main/java/com/example/jiangzehui/contentproviderdemo/MainActivity.java
+++ app/src/main/java/com/example/jiangzehui/contentproviderdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.jiangzehui.contentproviderdemo;
 
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -25,29 +28,30 @@ public class MainActivity extends AppCompatActivity {
     MyAdapter adapter;
     Random random;
     TextView tvTotal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        random= new Random();
-        list =  DBService.getInstence(this).search();
+        random = new Random();
+        list = DBService.getInstence(this).search();
         ListView lv = (ListView) findViewById(R.id.lv);
         tvTotal = (TextView) findViewById(R.id.tvTotal);
-        tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
+        tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
         adapter = new MyAdapter();
         lv.setAdapter(adapter);
         findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ContentValues values = new ContentValues();
-                values.put("name","jiangzehui"+random.nextInt(100));
-                values.put("age",20+random.nextInt(10));
-                if(DBService.getInstence(MainActivity.this).save(values)){
-                    list =  DBService.getInstence(MainActivity.this).search();
+                values.put("name", "jiangzehui" + random.nextInt(100));
+                values.put("age", 20 + random.nextInt(10));
+                if (DBService.getInstence(MainActivity.this).save(values)) {
+                    list = DBService.getInstence(MainActivity.this).search();
                     adapter.notifyDataSetChanged();
-                    tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
-                }else{
-                    Toast.makeText(MainActivity.this,"添加失败",Toast.LENGTH_SHORT).show();
+                    tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
+                } else {
+                    Toast.makeText(MainActivity.this, "添加失败", Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -56,21 +60,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 DBService.getInstence(MainActivity.this).deleteAll();
-                list =  DBService.getInstence(MainActivity.this).search();
+                list = DBService.getInstence(MainActivity.this).search();
                 adapter.notifyDataSetChanged();
-                tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
+                tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
             }
         });
         lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 try {
-                    if(DBService.getInstence(MainActivity.this).delete(list.get(position).getInt("id"))){
-                        list =  DBService.getInstence(MainActivity.this).search();
+                    if (DBService.getInstence(MainActivity.this).delete(list.get(position).getInt("id"))) {
+                        list = DBService.getInstence(MainActivity.this).search();
                         adapter.notifyDataSetChanged();
-                        tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
-                    }else{
-                        Toast.makeText(MainActivity.this,"删除失败",Toast.LENGTH_SHORT).show();
+                        tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
+                    } else {
+                        Toast.makeText(MainActivity.this, "删除失败", Toast.LENGTH_SHORT).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -85,40 +89,65 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ContentValues values = new ContentValues();
-                values.put("name","jiangzehui"+random.nextInt(100));
-                values.put("age",20+random.nextInt(10));
-                //getContentResolver().insert()
-                if(DBService.getInstence(MainActivity.this).save(values)){
-                    list =  DBService.getInstence(MainActivity.this).search();
-                    adapter.notifyDataSetChanged();
-                    tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
-                }else{
-                    Toast.makeText(MainActivity.this,"添加失败",Toast.LENGTH_SHORT).show();
+                values.put("name", "jiangzehui" + random.nextInt(100));
+                values.put("age", 20 + random.nextInt(10));
+                getContentResolver().insert(Uri.parse(MyContentProvider.URI_ALL), values);
+
+                Cursor cursor = getContentResolver().query(Uri.parse(MyContentProvider.URI_ALL), null, null, null, null);
+                list = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    JSONObject object = new JSONObject();
+                    int id = cursor.getInt(cursor.getColumnIndex("id"));
+                    int age = cursor.getInt(cursor.getColumnIndex("age"));
+                    String name = cursor.getString(cursor.getColumnIndex("name"));
+                    try {
+                        object.put("id", id);
+                        object.put("age", age);
+                        object.put("name", name);
+                        Log.d("object=", object.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    list.add(object);
                 }
+                adapter.notifyDataSetChanged();
+                tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
+
 
             }
         });
         findViewById(R.id.btnDel2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBService.getInstence(MainActivity.this).deleteAll();
-                list =  DBService.getInstence(MainActivity.this).search();
+
+                getContentResolver().delete(Uri.parse(MyContentProvider.URI_ALL), null, null);
+                Cursor cursor = getContentResolver().query(Uri.parse(MyContentProvider.URI_ALL), null, null, null, null);
+                list = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    JSONObject object = new JSONObject();
+                    int id = cursor.getInt(cursor.getColumnIndex("id"));
+                    int age = cursor.getInt(cursor.getColumnIndex("age"));
+                    String name = cursor.getString(cursor.getColumnIndex("name"));
+                    try {
+                        object.put("id", id);
+                        object.put("age", age);
+                        object.put("name", name);
+                        Log.d("object=", object.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    list.add(object);
+                }
                 adapter.notifyDataSetChanged();
-                tvTotal.setText("记录条数："+DBService.getInstence(MainActivity.this).getCount()+"");
+                tvTotal.setText("记录条数：" + DBService.getInstence(MainActivity.this).getCount() + "");
             }
         });
-
-
-
-
 
 
     }
 
 
-
-
-    class MyAdapter extends BaseAdapter{
+    class MyAdapter extends BaseAdapter {
         @Override
         public int getCount() {
             return list.size();
@@ -138,8 +167,8 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             TextView tv = new TextView(MainActivity.this);
             try {
-                tv.setText("id="+list.get(position).getString("id")+"  name="+list.get(position).getString("name")+"  age="+list.get(position).getString("age"));
-                tv.setPadding(20,20,20,20);
+                tv.setText("id=" + list.get(position).getString("id") + "  name=" + list.get(position).getString("name") + "  age=" + list.get(position).getString("age"));
+                tv.setPadding(20, 20, 20, 20);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
